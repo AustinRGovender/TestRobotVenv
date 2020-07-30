9872218705a63349ca6020b5de921030e403294c
AustinRGovender <austinryang1@gmail.com>
2020-07-30 10:59:05 +0200

    initial

---
+++ Page Objects/ApparelHomePage.py
@@ -0,0 +1,23 @@
+# Developed by: Austin Ryan Govender
+# Last-Modified: 15/07/2020
+# Purpose: This py file contains locators for the MRP Stage Home/Landing Page
+
+Browser = "chrome"
+eCommUrl = "https://mrp-stg-magento.sparkred.com/en_za/"
+
+CookieBannerCl = "css=#cookie-notice > div > div > span > a > span"
+CountrySelectBannerCl = "css=#header-country-selector > div > div > div > a"
+LoginOrRegOpt = "css=#header-mini-account > p > a"
+
+MrpLogo = "css=#header > div.container > div.row.menu-row-wrapper.bck-white > div.col-md-4.col-sm-4.col-xs-4.header-middle-logo-block > a"
+
+SignInButton = "css=#no-mini-profile > button.button.btn-raised"
+
+RegisterButton = "css=#no-mini-profile > button.button.btn-flat"
+
+MyProfileButton = "id=my-account"
+MyOrdersButton = "id=my-orders"
+ChangeCountryButton = "css=#mini\.account\.popup > ul > li:nth-child(3) > a"
+
+
+GuestCheckout = "id=js-vco-login-guest"
---
+++ Page Objects/SignInPage.py
@@ -0,0 +1,15 @@
+IsOnSignInPage = "css=#login-form > div.registered-users > div > h2"
+
+EmailTxt = "id=email"
+PasswordTxt = "id=pass"
+
+#RememberMeChk   ="id=remember_meyey3VyFJtd"
+RememberMeChk = "css#remember_meZkwFPn88UY"
+CaptchaBox = "id=recaptcha-anchor"
+
+SignInBtn = "id=send2"
+
+ForgotPassword = "css=#login-form > div.col-m > button.popup-window-btn.somePopup.button.btn-flat.right.waves-effect.waves-light"
+
+EmailAddressVal = "austinryang1@gmail.com"
+PasswordVal = "Price@10"
